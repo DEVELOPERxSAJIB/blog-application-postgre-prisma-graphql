76ef6a85460f287895bfc2d0f3df2012697f3118
developerxsajib <sajibshikder78971@gmail.com>
2024-12-25 02:58:29 +0600

    cors update

--- app/api/graphql/route.ts
+++ app/api/graphql/route.ts
@@ -1,8 +1,18 @@
+import Cors from 'cors';
 import { ApolloServer } from '@apollo/server';
 import { startServerAndCreateNextHandler } from '@as-integrations/next';
 import { typeDefs } from '@/graphql/typeDefs';
 import { resolvers } from '@/graphql/resolvers';
 import { createContext, Context } from '@/graphql/context';
+import initMiddleware from '@/lib/init-middleware';
+
+initMiddleware(
+  Cors({
+      methods: ['GET', 'POST', 'OPTIONS'],
+      origin: '*',
+  })
+);
+
 
 const apolloServer = new ApolloServer<Context>({
   typeDefs,
@@ -12,6 +22,7 @@ const apolloServer = new ApolloServer<Context>({
 const handler = startServerAndCreateNextHandler(apolloServer, {
   context: async (req, res) => createContext(req, res),
 });
+
 
 export const GET = handler;
 export const POST = handler;
